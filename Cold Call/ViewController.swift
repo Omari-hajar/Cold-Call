cbedde1ff49ff092b488d9cfadbd9a480bffbe5e
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-05 23:20:53 +0300

    edit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //declare UI elements
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    //declare + init variables
+    
+    var names: [String] = ["Mark", "Kalid", "Layla", "Sara", "Noor"]
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameLabel.text = "Unknown"
+      
     }
-
-
+    
+ 
+    @IBAction func coldCallBtn(_ sender: UIButton) {
+        nameLabel.text = names[Int.random(in: 0..<names.count)]
+        let randomNumber = Int.random(in: 1...5)
+        let strNum = String(randomNumber)
+        numberLabel.text = strNum
+        if randomNumber == 1 || randomNumber == 2 {
+          
+            numberLabel.textColor = UIColor.red
+        } else if randomNumber == 3 || randomNumber == 4{
+            numberLabel.textColor = UIColor.orange
+        } else{
+            numberLabel.textColor = UIColor.green
+        }
+    }
+    
+    
+    
 }
+
+
 
